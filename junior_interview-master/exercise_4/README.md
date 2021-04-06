f9aede301a73dc25d97ea90584684995c5ba16b8
Leonardo Capogna <leonardocapogna@gmail.com>
2021-04-06 19:03:48 +0200

    esercizio 5
    fixed

--- junior_interview-master/exercise_4/README.md
+++ junior_interview-master/exercise_4/README.md
@@ -7,3 +7,5 @@ client deve avere queste pagine:
 
 alla pagina admin si accedere solo se loggati. Nella pagina admin è presente solo un tasto per poter fare il logout e tornare quindi alla pagina di login. Gestisci il processo di autenticazione in autonomia tramite un form di login che chieda unicamente username e password. 
 
+Provando con co. non php
+
--- junior_interview-master/exercise_5/app.js
+++ junior_interview-master/exercise_5/app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', () => {
     var obj = [
         {
             name: 'Madagascar',
@@ -25,22 +25,71 @@ document.addEventListener("DOMContentLoaded", function() {
             type: 'South America',
         }
         ];
-        var btnField = document.getElementById('filterContent');
-        obj.forEach(element => {
-            btnField.innerHTML += '<button type="button" class="btn btn-primary btn-block">' + element['type'] +'</button>';
-        });
-        var cardContent = document.getElementById('cardContent');
-        obj.forEach(element => {
-            cardContent.innerHTML += '<div class="col"><div class="card shadow-sm"><div class="card-body"><h5 class="card-title">' + element['name'] + '</h5> <p class="card-text">' + element['type'] + '</p></div></div></div>';
-        });
-
-
-        // var btn = document.querySelector('button');
-        // btn.addEventListener("click", function(e){
-        //     var target = e.target;
-        //     // var card = document.querySelector('.card');
-        //     if (target.textContent === "Asia") {
-        //         alert('prova')
-        //      }
-        //    });
+    var filteredType = [];
+    var buttonContainer = document.querySelector('#filterContent');
+    var cardContent = document.querySelector('#cardContent');
+    //stampo i buttons
+    obj.forEach(element => {
+        //all'inizio li stampo tutti
+        cardContent.innerHTML += `
+            <div class="col">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.name}</h5>
+                        <p class="card-text">${element.type}</p>
+                    </div>
+                </div>
+            </div> 
+        `;
+        //creo i buttons univoci
+        if (!filteredType.includes(element.type)) {
+            filteredType.push(element.type);
+            var newButton = document.createElement('button');
+            newButton.innerHTML = element.type;
+            buttonContainer.appendChild(newButton);
+            //gestisco al click
+        }
     });
+    //gestisco il click dei buttons
+    var buttons = Array.from(buttonContainer.children);
+    buttons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                if (e.target.innerHTML == 'All') {
+                    cardContent.innerHTML = '';
+                    obj.forEach(element => {
+                        //li stampo tutti
+                        cardContent.innerHTML += `
+                            <div class="col">
+                                <div class="card shadow-sm">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${element.name}</h5>
+                                        <p class="card-text">${element.type}</p>
+                                    </div>
+                                </div>
+                            </div> 
+                        `;
+                    });
+                } else {
+                //creo l'array filtrato
+                var filteredArray = obj.filter(album => {
+                    return album.type == e.target.innerHTML;
+                });
+                //svuoto il cardContent
+                cardContent.innerHTML = '';
+                //stampo gli elementi dell'array
+                filteredArray.forEach(element => {
+                    cardContent.innerHTML += `
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <div class="card-body">
+                                <h5 class="card-title">${element.name}</h5>
+                                <p class="card-text">${element.type}</p>
+                            </div>
+                        </div>
+                    </div> 
+                    `;
+                })
+            }
+        });
+    });
+});
